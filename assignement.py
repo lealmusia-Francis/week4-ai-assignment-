c9344c8254cf8acf31cda29bad3bceba0329dc15
lealmusia-Francis <lealmusiafrank1@gmail.com>
2026-03-13 23:51:09 +0300

    Create assignement.py

---
+++ assignement.py
@@ -0,0 +1,24 @@
+##Qestion 4
+print("Hello AI stdents")
+print
+
+##Question 5
+name="John"
+age=20
+city="Nairobi"
+print("Name:", name)
+print("Age:", age)
+print("City:", city)
+
+##Question 6
+Programming=("java","Python","Javascscript","C++","G0")
+print(Programming[0])
+
+##Question 7
+student = {
+    "name": "John",
+    "age": 21,
+    "course": "AI"
+}
+
+print(student["name"])
